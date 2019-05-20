d8a81eb2e833463f744ffccca25b01c3435f295b
Nader Arbabian <Nader.arb72@gmail.com>
2019-05-20 19:26:03 -0400

    initial commit

---
+++ users/models.py
@@ -0,0 +1,7 @@
+from django.db import models
+from django.contrib.auth.models import AbstractBaseUser
+
+
+class CustomUser(AbstractBaseUser):
+    email = models.EmailField(unique=True)
+    USERNAME_FIELD = 'email'
