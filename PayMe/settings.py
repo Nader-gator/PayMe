bca3dffb6ee1d578ffbb47f3fbccaff6583dda3c
Nader Arbabian <Nader.arb72@gmail.com>
2019-05-23 21:09:16 -0400

    add heroku env variables

--- PayMe/settings.py
+++ PayMe/settings.py
@@ -111,10 +111,10 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
-
+HEROKU = os.environ.get('heroku', False)
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
-if not DEBUG:
+if HEROKU:
     import django_heroku
     django_heroku.settings(locals())
 
